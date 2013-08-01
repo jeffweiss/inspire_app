1990dcffd1325cf4439bcfb259f068bc873cfd01
Jeff Weiss <jeff.weiss@puppetlabs.com>
2013-08-01 12:03:29 -0700

    initial rails 3.2.14 project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+InspireApp::Application.config.secret_token = 'ba451c30b8563694a827487f35d758d6b976d5497bff4bb5431807d19f7e2aa11d7c2bb428726069c3b76c9a9dfd3840a60724bf664c750ff716edbfd8277cbb'
